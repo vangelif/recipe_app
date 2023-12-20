3bf4ecf3c3cb93cd3e363196520cc1a610039fa0
vangelif <ev.fotiades@yahoo.com>
2023-12-20 16:24:53 +0200

    config recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,22 @@
 class RecipesController < ApplicationController
+  # root page, /recipes
   def index; end
+
+  # ../recipes/1 
+  def show; end
+
+  # ../recipes/new
+  def new; end
+
+  # ../recipes (POST)
+  def create; end
+
+  # ../recipes/1/edit
+  def edit; end
+
+  # ../recipes/1 (PATCH/PUT)
+  def update; end
+
+  # ../recipes/1 (DELETE)
+  def destroy; end
 end
