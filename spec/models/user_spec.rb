6c77c0d20ac24b66bf043f064dc355236c9b6c02
vangelif <ev.fotiades@yahoo.com>
2023-12-20 12:35:15 +0200

    build tests for user, recipe, food models

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+    subject { User.new(name: "jeremy") }
+    before { subject.save }
+
+        it "name must not be blank" do
+        expect(subject).not_to be_valid
+        end
+    end
+# end
+# end
