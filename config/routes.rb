86cad50d185e669564c6207c5bca37818a90167e
vangelif <ev.fotiades@yahoo.com>
2023-12-21 13:23:49 +0200

    build routes for recipes and public recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'recipes/index'
+  # get 'recipes/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'recipes#index'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
   resources :foods, only: %i[index show new create destroy]
-  resources :recipes, only: %i[index show new create destroy]
-  resources :recipe_foods, only: %i[create destroy]
-  resources :public_recipes, only: %i[index]
+  resources :recipes, only: %i[index show new create destroy] do
+    collection do
+      get :public_recipes
+  end
+end
+  get 'public_recipes', to: 'recipes#public_recipes'
+  
+  
   resources :general_shopping_list, only: %i[index]
   # Defines the root path route ("/")
   # root "posts#index"
+  get "up" => "rails/health#show", as: :rails_health_check
 end
