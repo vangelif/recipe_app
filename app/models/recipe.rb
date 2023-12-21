155705e9b6663430cbba25ae15b1f22d430ede6b
vangelif <ev.fotiades@yahoo.com>
2023-12-21 20:22:23 +0200

    build public recipes to display all of them and lead to recipe details

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,12 @@ class Recipe < ApplicationRecord
   validates :name, :preparation_time, :cooking_time, presence: true
   # validates :name, uniqueness: true
   validates :preparation_time, :cooking_time, numericality: { only_integer: true }
+
+  def self.public_recipes
+    joins(:author, :foods)
+      .select('recipes.id, recipes.name AS recipe_name, users.name AS user_name, users.email AS user_email, COUNT(foods.id) AS total_food_items, SUM(foods.price) AS total_food_price')
+      .where(public: true)
+      .group('recipes.id, author.id')
+      .order('recipes.id')
+  end
 end
