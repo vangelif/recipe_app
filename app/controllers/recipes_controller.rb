2a0fa88bc9525cffee9b203125b4a32280fd2824
vangelif <ev.fotiades@yahoo.com>
2023-12-21 13:21:51 +0200

    refactor actions of controller to ensure logged in user is current_user, build create and destroy actions as well

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,15 +1,32 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :new, :create, :edit, :update, :destroy]
+
   # root page, /recipes
-  def index; end
+  def index
+    @recipes = current_user.recipes
+  end
 
   # ../recipes/1 
-  def show; end
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
 
   # ../recipes/new
-  def new; end
+  def new
+    @recipe = Recipe.new
+  end
 
   # ../recipes (POST)
-  def create; end
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.author = current_user
+    if @recipe.save
+      redirect_to recipes_path, notice: "Recipe was successfully created."
+    else
+      puts @recipe.errors.full_messages
+      render :new
+    end
+  end
 
   # ../recipes/1/edit
   def edit; end
@@ -18,5 +35,19 @@ class RecipesController < ApplicationController
   def update; end
 
   # ../recipes/1 (DELETE)
-  def destroy; end
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path, notice: "Recipe was successfully deleted."
+  end
+
+  def public_recipes
+    @public_recipes = Recipe.where(public: true)
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :preparation_time, :cooking_time, :public)
+  end
 end
