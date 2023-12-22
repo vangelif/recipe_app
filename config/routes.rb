f1131cece54fe0507850ff5be78c8f1bf3533146
HenaryKhuraijam <henarykhu123@gmail.com>
2023-12-22 10:37:06 +0530

    ingredient

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   # get 'recipes/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'recipes#index'
+  root to: 'recipes#index'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  resources :foods, only: %i[index show new create destroy]
+  # resources :foods, only: %i[index show new create destroy]
   resources :recipes, only: %i[index show new create destroy] do
     collection do
       get :public_recipes
       get :generate_shopping_list
+    end
   end
-end
-  get 'public_recipes', to: 'recipes#public_recipes'
-  
-  
-  resources :general_shopping_list, only: %i[index]
-  # Defines the root path route ("/")
-  # root "posts#index"
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :foods, except: [:edit, :update]
+
+  get '/general_shopping_list', to: 'foods#general'
+  get '/public_recipes', to: 'recipes#public'
 end
