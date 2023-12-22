9653bc207a39c38731ddc1feb3b8a996d6db7221
HenaryKhuraijam <henarykhu123@gmail.com>
2023-12-22 19:32:28 +0530

    Update Recipe Show Generate Shopping list and Add Ingredient funcionality

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :foods, through: :ingredients
   has_many :ingredients
+  has_many :foods, through: :ingredients
 
   validates :name, :preparation_time, :cooking_time, presence: true
   # validates :name, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # resources :foods, only: %i[index show new create destroy]
   resources :recipes, only: %i[index show new create destroy] do
+    resources :ingredients, only: [:new, :create, :destroy]    
     collection do
       get :public_recipes
       get :generate_shopping_list
@@ -15,5 +16,5 @@ Rails.application.routes.draw do
   resources :foods, except: [:edit, :update]
 
   get '/general_shopping_list', to: 'foods#general'
-  get '/public_recipes', to: 'recipes#public'
+  get 'public_recipes', to: 'recipes#public_recipes'
 end
